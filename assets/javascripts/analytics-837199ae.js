08805f7559b9fa0aebe5c9c97e799f5dc3da2e31
jereksel <jereksel@gmail.com>
2020-05-30 23:30:52 +0000

    Deploying to gh-pages from  @ 3d79dcd052048caaea1dd702eb655d1acb49302e 🚀

---
+++ assets/javascripts/analytics-837199ae.js
@@ -0,0 +1,28 @@
+document.addEventListener('turbolinks:load', function() {
+  analytics.page()
+
+  track('.downloads .download .details li a', function(el) {
+    var m = el.href.match(/terraform_(.*?)_(.*?)_(.*?)\.zip/)
+    return {
+      event: 'Download',
+      category: 'Button',
+      label: 'Terraform | v' + m[1] + ' | ' + m[2] + ' | ' + m[3],
+      version: m[1],
+      os: m[2],
+      architecture: m[3],
+      product: 'terraform'
+    }
+  })
+
+  track('a.notification', function(el) {
+    var params = {
+      name: 'Alert Banner',
+      variant: el.innerText.replace(/\s+/g, ' ').trim()
+    }
+    // Create a stringified `label` prop of the event for GA tracking purposes
+    params.label = JSON.stringify(params)
+    params.event = 'CTA Clicked'
+    return params
+  })
+})
+;
